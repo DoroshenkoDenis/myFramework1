47d7d5b6310c51d6b449be04731e2f04e8efc8dc
Denis Doroshenko <ddtlt1984@gmail.com>
2022-07-06 03:52:35 +0400

    added yaSearchTest

--- src/test/java/com/framework/dd/pages/ya/YaHomePage.java
+++ src/test/java/com/framework/dd/pages/ya/YaHomePage.java
@@ -10,10 +10,13 @@ public class YaHomePage extends BasePage {
         super(driver);
     }
 
-    private final By submitBtn = By.xpath("//*/button[@type='submit']");
+//    private final By submitBtn = By.xpath("//*/button[@type='submit']");
+
+
 
     public void clickSearch() {
-        driver.findElement(submitBtn).click();
+//        driver.findElement(submitBtn).click();
+        driver.findElement(By.cssSelector("div.search3__inner > button[class='search3__button mini-suggest__button']")).click();
     }
 
 }
